740f9db3fd624f93008b00af572209046d7c7d2c
Sam Turner (MP) <sam@digi.ltd.uk>
2016-02-06 20:15:31 +0000

    Squashed commit of the following:
    commit 0c3ac6a08b37c43c5ddcb0a55e9b795a97b06fb8
    Author: Sam Turner (MP) <sam@digi.ltd.uk>
    Date:   Sat Feb 6 20:14:05 2016 +0000
    
        version bump
    
    commit 53556032034a98836588e6729f3c15ba33e6ba97
    Author: Sam Turner (MP) <sam@digi.ltd.uk>
    Date:   Sat Feb 6 20:11:53 2016 +0000
    
        Merged pages into one html file with tabbed view and added an extra setting for the Admin url
    
    commit b168bad2ba95441da06482c0798344142f03d359
    Author: Sam Turner (MP) <sam@digi.ltd.uk>
    Date:   Wed Feb 3 16:55:23 2016 +0000
    
        added a var for the "admin" link that wasn't working

--- js/settings.js
+++ js/settings.js
@@ -2,9 +2,11 @@
 function save_options() {
   var piholeurl = document.getElementById('piholeurl').value;
   var piholekey = document.getElementById('piholekey').value;
+  var pihole_local_admin = document.getElementById('pihole_local_admin').value;
   chrome.storage.sync.set({
     piholekey: piholekey,
-    piholeurl: piholeurl
+    piholeurl: piholeurl,
+    pihole_local_admin: pihole_local_admin
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -15,15 +17,17 @@ function save_options() {
   });
 }
 
-// Localstorage 
+// Localstorage
 function restore_options() {
   // Use defaults
   chrome.storage.sync.get({
     piholekey: '123456789',
-    piholeurl: 'http://raspberrypi.local/admin/apiext.php'
+    piholeurl: 'http://raspberrypi.local/admin/apiext.php',
+    pihole_local_admin: 'http://raspberrypi.local/admin/index.php'
   }, function(items) {
     document.getElementById('piholeurl').value = items.piholeurl;
     document.getElementById('piholekey').value = items.piholekey;
+    document.getElementById('pihole_local_admin').value = items.pihole_local_admin;
   });
 }
 
